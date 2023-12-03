32c3d53afd71c2afe47eec5705e82c558888fec7
Werick da Silva <werick.s.santana34@gmail.com>
2023-12-03 17:41:06 -0300

    page mobile

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ["Inter", ...defaultTheme.fontFamily.sans]
+			},
+			textColor: {
+				'gray-light': '#666666'
+			},
+			backgroundColor: {
+				'card-news': '#161616',
+				'footer': '#F1F1F1'
+			},
+			borderColor: {
+				cus_gray: '#C7C7C7'
+			}
+		},
+	},
+	plugins: [],
+}
